408d7c3e53c33160b198456e27cb522b5a1e5a6c
nam nguyen <ntnam8980@gmail.com>
2017-09-08 16:24:07 +0700

    update session8

---
+++ session8/common.js
@@ -0,0 +1,21 @@
+var x = 5, y = 9;
+function multi(p1, p2) {
+	return p1 * p2;
+}
+function devi(p1, p2) {
+	return p1 / p2;
+}
+function plus(p1, p2) {
+	return p1 + p2;
+}
+function sub(p1, p2) {
+	return p1 - p2;
+}
+function mod(p1, p2) {
+	return p1 % p2;
+}
+document.write(multi(x, y), "<br>"); 
+document.write(devi(x, y), "<br>");
+document.write(plus(x, y), "<br>");
+document.write(sub(x, y), "<br>");
+document.write(mod(x, y), "<br>");
